ce820d3c29576ba822d39be660ccbeb2f7584f93
Reed-Carter <reedandrew18@gmail.com>
2022-11-04 09:05:23 -0800

    complete function dict_sayer

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ def list_sayer(list_one):
     for index in range(len(list_one)):
       print(f"index {index} is the value: {list_one[index]}")
     return True  
-
 list_sayer(list_one)
 
+
+dict_one = {"Burkina_Faso": "Ouagadougou", "Kazakhstan": "Nur-Sultan", "Japan": "Tokyo", "Peru": "Lima"}
+
+def dict_sayer(dict_one):
+  if len(dict_one) == 0:
+    print("The dictionary is empty!")
+    return False
+  else:
+    for key, value in dict_one.items():
+      print(f"the capital of {key} is {value}")
+    return True
+
+dict_sayer(dict_one)
